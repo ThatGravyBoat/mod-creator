1b49d54b6229386e8136c55911f1fe06fefe6793
ThatGravyBoat <thatgravyboat@gmail.com>
2023-05-30 16:57:27 -0230

    Fix forge tabbing

--- scripts/forge.js
+++ scripts/forge.js
@@ -1,19 +1,17 @@
 
-function createForgeJson(data) {
-    return `
-    modloader="lowcode"
-    loaderVersion="1"
-    license="${data.license}"
-    
-    [[mods]]
-    modId="${data.id}"
-    version="${data.version}"
-    displayName="${data.display.name}"
-    description="${data.display.description}"
-    ${data.display.icon ? `logoFile="${data.display.icon}"` : ""}
-    ${data.authors ? `authors="${getForgeAuthors(data.authors)}"` : ""}
-    `;
-}
+const createForgeJson = (data) => `
+modloader="lowcode"
+loaderVersion="1"
+license="${data.license}"
+
+[[mods]]
+modId="${data.id}"
+version="${data.version}"
+displayName="${data.display.name}"
+description="${data.display.description}"
+${data.display.icon ? `logoFile="${data.display.icon}"` : ""}
+${data.authors ? `authors="${getForgeAuthors(data.authors)}"` : ""}
+`;
 
 function getForgeAuthors(authors) {
     const result = [];
